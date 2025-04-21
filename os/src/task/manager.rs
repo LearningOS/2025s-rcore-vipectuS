e3d3adbec090fce51e7803ac7915745aaccddf52
vipectuS <vipectus014@gmail.com>
2025-04-21 11:53:59 +0800

    implement stride shedule

--- os/src/task/manager.rs
+++ os/src/task/manager.rs
@@ -23,7 +23,20 @@ impl TaskManager {
     }
     /// Take a process out of the ready queue
     pub fn fetch(&mut self) -> Option<Arc<TaskControlBlock>> {
-        self.ready_queue.pop_front()
+        let idx = self
+            .ready_queue
+            .iter()
+            .enumerate()
+            .min_by_key(|(_, task)| task.inner_exclusive_access().stride)
+            .map(|(index, _)| index)?;
+
+        let task = self.ready_queue.remove(idx).unwrap();
+        {
+            let mut inner = task.inner_exclusive_access();
+            inner.stride += 1_000_000 / inner.prio;
+        }
+
+        Some(task)
     }
 }
 
